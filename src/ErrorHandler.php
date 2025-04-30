cab8aa002eef598fe036e45dae52a8a9925da28f
marcoAS <mascarnatto@dpsonline.com.ar>
2025-04-30 11:15:09 -0300

    Update ErrorHandler.php

--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -172,7 +172,7 @@ class ErrorHandler
         $path = env::get('PATH_LOG', '/tmp') . date('/Y/m/d');
     
         if( ! is_dir( $path ) )
-          mkdir( $path, 0755, true);        
+          @mkdir( $path, 0755, true);        
         
         $this->log_path = $path;       
     }
